be4646bd7b84e11bac846b1e5d231b89b8699fe1
GPICCOL <GPICCOLI@CCT.LSU.EDU>
2017-02-04 10:18:08 -0600

    Completed the layout of question page. No interactivity yet

--- myapp/routes/generic.js
+++ myapp/routes/generic.js
@@ -5,9 +5,10 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('generic', { 
   	title: 'Question Page',
-  	area: 'This is the Topic Area e.g., Chapter1 Hardware',
-  	topic: 'This is the topic e.g., Microprocessors',
-    question: 'This is the actual question e.g., what are the elements of a CPU'
+  	area: 'Chapter1: Hardware',
+  	topic: 'Microprocessors',
+    question: 'This is the actual question e.g., what are the elements of a CPU',
+    correct: 'This is the correct answer and maybe some explanation.'
   });
 });
 
