7c5540348b08eb44a0644f9f2287c7c62f4311b5
GPICCOL <GPICCOLI@CCT.LSU.EDU>
2017-02-03 18:08:17 -0600

    Fixed class and id mistake

--- myapp/routes/generic.js
+++ myapp/routes/generic.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* Render the Generic Template. */
 router.get('/', function(req, res, next) {
-  res.render('generic', { title: 'Gabe\'s Generic Template' });
+  res.render('generic', { 
+  	title: 'Question Page',
+  	area: 'This is the Topic Area e.g., Chapter1 Hardware',
+  	topic: 'This is the topic e.g., Microprocessors',
+    question: 'This is the actual question e.g., what are the elements of a CPU'
+  });
 });
 
 module.exports = router;
