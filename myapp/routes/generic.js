2f8715b71193ff719e63ced115f9c89c55a647a7
GPICCOL <GPICCOLI@CCT.LSU.EDU>
2017-02-06 10:55:41 -0600

    Started reading from JASON files

--- myapp/routes/generic.js
+++ myapp/routes/generic.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var router = express.Router();
-
+//var myArr = JSON.parse(../bin/questions.json);
+var obj = JSON.parse(
+	'{"area": "JSON Chapter1: Hardware", "topic": "JSON Microprocessors", "question": "JSON This is the actual HARDWARE question e.g., what are the elements of a CPU", "answer": "JSON This is the correct HARDWARE answer and maybe some explanation."}'
+	);
 /* Render the Generic Template. */
 router.get('/', function(req, res, next) {
   res.render('generic', { 
   	title: 'Question Page',
-  	area: 'Chapter1: Hardware',
-  	topic: 'Microprocessors',
-    question: 'This is the actual question e.g., what are the elements of a CPU',
-    correct: 'This is the correct answer and maybe some explanation.'
+  	area: obj.area,
+  	topic: obj.topic,
+    question: obj.question,
+    correct: obj.answer
   });
 });
 
